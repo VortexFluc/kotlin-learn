49b64a385fa2938bc122ac5749038f67a0098204
Andrey Mihaylin <look2009new@gmail.com>
2022-12-23 16:28:28 +0300

    OOP. Data-class.

---
+++ src/main/kotlin/oop/Address.kt
@@ -0,0 +1,48 @@
+package oop
+
+class Address (val city: String, val street: String, val houseNum: Int) {
+
+    fun copy(city: String = this.city, street: String = this.street, houseNum: Int = this.houseNum): Address {
+        return Address(city, street, houseNum)
+    }
+
+    operator fun component1() = city
+    operator fun component2() = street
+    operator fun component3() = houseNum
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Address
+
+        if (city != other.city) return false
+        if (street != other.street) return false
+        if (houseNum != other.houseNum) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = city.hashCode()
+        result = 31 * result + street.hashCode()
+        result = 31 * result + houseNum
+        return result
+    }
+
+}
+
+fun main() {
+    val address1 = Address("Moscow", "Zelenaya", 12)
+    val address2 = address1.copy()
+
+    println(address1)
+    println(address2)
+
+    println(address1.hashCode())
+    println(address2.hashCode())
+
+    println(address1 == address2)
+
+    val (city, street, houseNum) = address1
+}
---
+++ src/main/kotlin/oop/Student.kt
@@ -0,0 +1,31 @@
+package oop
+
+class Student(val name: String, val lastName: String, val id: Int) {
+    fun copy(name: String = this.name, lastName: String = this.lastName, id: Int = this.id): Student {
+        return Student(name, lastName, id)
+    }
+
+    operator fun component1() = name
+    operator fun component2() = lastName
+    operator fun component3() = id
+}
+
+fun main() {
+    val student1 = Student("Ivan", "Ivanov", 32154)
+    val student2 = student1.copy(id = 654321)
+    val (name, lastName, id) = student1
+
+    println(name)
+    println(lastName)
+    println(id)
+
+    println(student1)
+    println(student2)
+
+    println(student1.hashCode())
+    println(student2.hashCode())
+
+    println(student1 == student2)
+    println(student1 === student2)
+
+}
