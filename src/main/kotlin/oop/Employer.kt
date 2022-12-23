ca5787300fdc19cf4ee98461c4ec2b6d14ee4d6a
Andrey Mihaylin <look2009new@gmail.com>
2022-12-23 15:56:43 +0300

    OOP. Methods.

---
+++ src/main/kotlin/oop/Employer.kt
@@ -0,0 +1,22 @@
+package oop
+
+import java.time.LocalDate
+
+class Employer(val name: String, var position: String, val year: Int) {
+
+    val experience: Int
+    get() = LocalDate.now().year - year
+
+    fun work() {
+        println("Working...")
+    }
+}
+
+fun main(args: Array<String>) {
+    val employer = Employer("Andrey", "Backend", 2021)
+    employer.printInfo()
+}
+
+fun Employer.printInfo() {
+    println("Name: $name, Position: $position, Year: $year, Experience: $experience")
+}
---
+++ src/main/kotlin/oop/MethodEx.kt
@@ -0,0 +1,25 @@
+package oop
+
+class MethodEx(val name: String, var age: Int, val weight: Float = 0f) {
+
+    val isOld: Boolean
+    get() = age >= 12
+
+    fun printInfo() {
+        println("Name: $name, Age: $age, Weight: $weight")
+    }
+}
+
+fun main() {
+    val test = MethodEx("Test", 1234, 54f)
+    test.printInfo()
+    println(test.isOld)
+
+    test.age = 10
+    println(test.isOld)
+    test.hello()
+}
+
+fun MethodEx.hello() {
+    println("Hello!")
+}
